70119862d0efa169375da78329fab5ceb6daa946
Medanaee <medanaee@gmail.com>
2026-05-28 16:19:45 +0330

    add worker

--- vite.config.ts
+++ vite.config.ts
@@ -10,11 +10,5 @@ import { cloudflare } from "@cloudflare/vite-plugin";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), viteSingleFile(), cloudflare()],
-  build: {
-    // اسم پوشه جدیدی که می‌خواهی ساخته شود را اینجا بنویس
-    outDir: 'build', 
-    
-    // (اختیاری) این گزینه باعث می‌شود قبل از هر بیلد جدید، محتویات پوشه قبلی پاک شود
-    emptyOutDir: true 
-  }
+  
 })
